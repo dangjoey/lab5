25c6bb9a5d907e7210fad2fc25b909754fd05666
Joey Dang <jodang@ucsd.edu>
2021-02-01 19:01:51 -0800

    Add friend route

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-
 /**
- * Module dependencies.
- */
-
+* Module dependencies.
+*/
+ 
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
-
+ 
 var index = require('./routes/index');
+var add = require('./routes/add');
 // Example route
 // var user = require('./routes/user');
-
+ 
 var app = express();
-
+ 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -28,17 +28,18 @@ app.use(express.cookieParser('Intro HCI secret key'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+ app.use(express.errorHandler());
 }
-
+ 
 // Add routes here
 app.get('/', index.view);
+app.get('/add', add.addFriend);
 // Example route
 // app.get('/users', user.list);
-
+ 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+ console.log('Express server listening on port ' + app.get('port'));
 });
