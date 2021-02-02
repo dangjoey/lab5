9818bf0de708bef4d579faa36b4b218f523d8612
Joey Dang <jodang@ucsd.edu>
2021-02-01 21:26:08 -0800

    Add new friend route

--- routes/add.js
+++ routes/add.js
@@ -1,9 +1,9 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {
-  let newFriend = {
+  var newFriend = {
     "name": request.query.name,
-    "name": request.query.name,
+    "description": request.query.name,
     "imageURL": "http://lorempixel.com/400/400/people/"
   }
   data.friends.push(newFriend);
