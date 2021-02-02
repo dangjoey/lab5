c9bb8dba17a01eb6f099facfc324692d1fd8591f
Joey Dang <jodang@ucsd.edu>
2021-02-01 19:02:21 -0800

    Update routes

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
-
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+ 
+exports.addFriend = function(request, response) {
+ // Your code goes here
+ let newFriend = {
+   "name": request.query.name,
+   "description": request.query.description,
+   "imageURL": "http://lorempixel.com/400/400/people"
+ }
+ data.friends.push(newFriend);
+ response.render('index', data);
+ console.log("done");
+}
